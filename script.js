bf7864370559da5ec079be7c0460559860289a6b
Jarrett <54005487+meatloaf85@users.noreply.github.com>
2021-03-24 00:32:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const cluePauseTime = 333 // how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global variables
-var pattern = [0,0,0,0,0,0,0,0,0,0];
+
+var pattern = [];
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
@@ -14,42 +15,144 @@ var number = 0;
 var clueHoldTime = 1000; //how long to hold each clue's light/sound
 var mistakes = 0;
 var tries = 3;
+var easy = 0;
+var normal = 0;
+var hard = 0;
+
+function easyMode(){
+  pattern = [0,0,0,0];
+  tries = 5;
+  clueHoldTime = 1500; 
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("gameButtonAreaEasy").classList.remove("hidden");
+  document.getElementById("normalBtn").classList.add("hidden");
+  document.getElementById("hardBtn").classList.add("hidden");
+  document.getElementById("play").classList.remove("hidden");
+  document.getElementById("diff").classList.add("hidden");
+  
+  easy++;
+}
+
+function normalMode(){
+  pattern = [0,0,0,0,0,0,0];
+  tries = 3;
+  clueHoldTime = 1000; 
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("gameButtonAreaNormal").classList.remove("hidden");
+  document.getElementById("easyBtn").classList.add("hidden");
+  document.getElementById("hardBtn").classList.add("hidden");
+  document.getElementById("play").classList.remove("hidden");
+  document.getElementById("diff").classList.add("hidden");
+  normal++;
+}
+
+function hardMode(){
+  pattern = [0,0,0,0,0,0,0,0,0,0];
+  tries = 1;
+  clueHoldTime = 500; 
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("gameButtonAreaHard").classList.remove("hidden");
+  document.getElementById("easyBtn").classList.add("hidden");
+  document.getElementById("normalBtn").classList.add("hidden");
+  document.getElementById("play").classList.remove("hidden");
+  document.getElementById("diff").classList.add("hidden");
+  hard++;
+}
 
 function startGame(){
+  document.getElementById("play").classList.add("hidden");
   //intialize game variables
   mistakes = 0;
-  tries = 3;
+  //tries = 3;
   progress = 0;
   gamePlaying = true;
+  if (easy == 1){
+  for(let j=0;j<pattern.length;j++){
+    number = Math.floor(Math.random() * 4)
+    while(number == 0){
+      number = Math.floor(Math.random() * 4)
+    }
+    pattern[j] = number
+  }
+  }
+  else if (normal == 1){
+  for(let j=0;j<pattern.length;j++){
+    number = Math.floor(Math.random() * 5)
+    while(number == 0){
+      number = Math.floor(Math.random() * 5)
+    }
+    pattern[j] = number
+  }
+  }
+  else if (hard == 1){
   for(let j=0;j<pattern.length;j++){
     number = Math.floor(Math.random() * 7)
     while(number == 0){
       number = Math.floor(Math.random() * 7)
     }
     pattern[j] = number
+  }
+    
     //console.log(pattern[j])
   }
   // swap the start and stop buttons
+  document.getElementById("myTries").classList.remove("hidden");
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
-  document.getElementById("myTries").innerHTML = "You have " + tries + " tries remaining.";
+  document.getElementById("easyBtn").classList.add("hidden");
+  document.getElementById("normalBtn").classList.add("hidden");
+  document.getElementById("hardBtn").classList.add("hidden");
+  if (hard == 1) {
+    document.getElementById("myTries").innerHTML = "You have " + tries + " try remaining.";
+  } else{
+    document.getElementById("myTries").innerHTML = "You have " + tries + " tries remaining.";
+  }
+  
   playClueSequence();
 }
 
 function stopGame(){
   //intialize game variables
   gamePlaying = false;
+  easy = 0;
+  normal = 0;
+  hard = 0;
+  number = 0;
   // swap the start and stop buttons
-document.getElementById("startBtn").classList.remove("hidden");
-document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("easyBtn").classList.remove("hidden");
+  document.getElementById("normalBtn").classList.remove("hidden");
+  document.getElementById("hardBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("myTries").classList.add("hidden");
+  document.getElementById("diff").classList.remove("hidden");
+  document.getElementById("gameButtonAreaHard").classList.add("hidden");
+  document.getElementById("gameButtonAreaNormal").classList.add("hidden");
+  document.getElementById("gameButtonAreaEasy").classList.add("hidden");
 }
 
 function lightButton(btn){
-  document.getElementById("button" + btn).classList.add("lit")
+  if (easy == 1){
+    document.getElementById("easyButton" + btn).classList.add("lit")
+  }
+  else if (normal == 1) {
+    document.getElementById("normalButton" + btn).classList.add("lit")
+  }
+  else if (hard == 1) {
+    document.getElementById("hardButton" + btn).classList.add("lit")
+  }
 }
 
 function clearButton(btn){
-  document.getElementById("button" + btn).classList.remove("lit")
+  if (easy == 1){
+    document.getElementById("easyButton" + btn).classList.remove("lit")
+  }
+  else if (normal == 1) {
+    document.getElementById("normalButton" + btn).classList.remove("lit")
+  }
+  else if (hard == 1) {
+    document.getElementById("hardButton" + btn).classList.remove("lit")
+  }
 }
 
 function playSingleClue(btn){
@@ -103,22 +206,49 @@ function guess(btn){
       guessCounter++;
     }
   }else{
-    mistakes += 1;
+    if (easy == 1) {
+      mistakes += 1;
     //guess incorrect
     //gameover
-    if(mistakes < 2){
+    if (mistakes < 4){
       tries -= 1;
       document.getElementById("myTries").innerHTML = "You have " + tries + " tries remaining.";
       //alert("You have " + tries + " tries remaining");
     }
-    if(mistakes == 2){
+    else if (mistakes == 4){
       tries -= 1;
       document.getElementById("myTries").innerHTML = "You have " + tries + " try remaining.";
       //alert("You have " + tries + " try remaining");
     }
-    if(mistakes == 3){
+    else if (mistakes == 5){
       document.getElementById("myTries").innerHTML = "You have " + 0 + " tries remaining. Game over!";
       loseGame();
+    }
+    }
+    
+    if (normal == 1) {
+    mistakes += 1;
+    //guess incorrect
+    //gameover
+    if (mistakes < 2){
+      tries -= 1;
+      document.getElementById("myTries").innerHTML = "You have " + tries + " tries remaining.";
+      //alert("You have " + tries + " tries remaining");
+    }
+    else if (mistakes == 2){
+      tries -= 1;
+      document.getElementById("myTries").innerHTML = "You have " + tries + " try remaining.";
+      //alert("You have " + tries + " try remaining");
+    }
+    else if (mistakes == 3){
+      document.getElementById("myTries").innerHTML = "You have " + 0 + " tries remaining. Game over!";
+      loseGame();
+    }
+    }
+    
+    if (hard == 1) {
+    mistakes += 1;
+    loseGame();
     }
     
   }
